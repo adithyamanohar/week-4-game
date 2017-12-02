e1a3f71c62143716436744597756b46ddfc7ac95
Adithya Manohar <manohar.adithya@gmail.com>
2017-12-02 18:27:54 -0500

    2

---
+++ assets/javascript/game.js
@@ -0,0 +1,193 @@
+$( document ).ready(function(){
+ 
+
+
+  var randomNum= 20
+  // Selects a random number to be shown at the start of the game
+  // Number should be should be between 19 - 120
+  // Display the random number in the webpage
+  $('#numberToGet').text(randomNum);
+  // display the random number in the console log
+  console.log("The Number to get is: " + randomNum);
+
+// Set up a math.random for each crystal between 1-12. They should have the same value.
+
+var bluenum = 5;
+
+	var greennum = 5;
+
+	var rednum = 5;
+
+	var yellownum = 5;
+
+
+// console log each crystal number 
+
+console.log('blue crystal is :' + bluenum);
+console.log('green crystal is :' + greennum);
+console.log('red crystal is :' + rednum);
+console.log('yellow crystal is :' + yellownum);
+
+
+// user total variable
+
+var userTotal = 0;
+
+$("#userTotal1").text(userTotal);
+
+//number of wins
+
+var playerWins = 0;
+
+//number of losses
+
+var playerLosses = 0;
+
+// display the wins and losses in the div
+$('#playerWins').text(playerLosses);
+
+$('#playerLosses').text(playerWins);
+
+
+
+// create a function to restart the game when player reaches the end, win or lose
+
+function gameReset (){
+	// Reset the User total
+	var userTotal = 0;
+	$("#userTotal1").text(userTotal);
+
+	// reset all of the randomly generated numbers by generating new ones
+	var randomNum=20
+	$('#numberToGet').text(randomNum);
+
+	var bluenum = 5;
+
+	var greennum = 5;
+
+	var rednum = 5;
+
+	var yellownum = 5;
+
+
+	// Console log each new number. just copy and paste honestly
+
+	console.log("The Number to get is: " + randomNum);
+	console.log('blue crystal is :' + bluenum);
+	console.log('green crystal is :' + greennum);
+	console.log('red crystal is :' + rednum);
+	console.log('yellow crystal is :' + yellownum);
+}
+
+// create function to run if usertotal = numberToGet; aka player wins
+
+function ifThePlayerWins(){
+	
+	playerWins++;
+	$('#playerWins').text(playerWins);
+	console.log (playerWins);
+	alert("Winner");
+	
+}
+
+// create a function to run if the usertotal > numberToGet; aka player losses
+
+function ifThePlayerLosses (){
+	
+	playerLosses++;
+	$('#playerLosses').text(playerLosses);
+	console.log(playerLosses);
+	alert("Loser");
+	
+}
+
+
+// set up what happens when the player clicks on the blue crystal image
+
+$('#blue').on('click', function() {
+
+	// add bluenum to the user total
+	userTotal = userTotal + bluenum;
+	//console log the new total from adding blue num
+	console.log(userTotal);
+
+	// Display the new user total
+	$("#userTotal1").text(userTotal);
+
+	if (userTotal === randomNum) {
+		ifThePlayerWins();
+		gameReset();
+	}
+	else if (userTotal > randomNum) {
+		ifThePlayerLosses();
+		gameReset();
+	}
+
+});
+
+// copy and paste for each pic
+$('#green').on('click', function() {
+
+	// add greennum to the user total
+	userTotal = userTotal + greennum;
+	//console log the new total from adding green num
+	console.log(userTotal);
+
+	// Display the new user total
+	$("#userTotal1").text(userTotal);
+
+	if (userTotal === randomNum) {
+		ifThePlayerWins();
+		gameReset();
+	}
+	else if (userTotal > randomNum) {
+		ifThePlayerLosses();
+		gameReset();
+	}
+
+});
+
+$('#red').on('click', function() {
+
+	// add rednum to the user total
+	userTotal = userTotal + rednum;
+	//console log the new total from adding red num
+	console.log(userTotal);
+
+	// Display the new user total
+	$("#userTotal1").text(userTotal);
+
+	if (userTotal === randomNum) {
+		ifThePlayerWins();
+		gameReset();
+	}
+	else if (userTotal > randomNum) {
+		ifThePlayerLosses();
+		gameReset();
+	}
+
+});
+
+$('#yellow').on('click', function() {
+
+	// add yellownum to the user total
+	userTotal = userTotal + yellownum;
+	//console log the new total from adding yellownum
+	console.log(userTotal);
+
+	// Display the new user total
+	$("#userTotal1").text(userTotal);
+
+	if (userTotal === randomNum) {
+		ifThePlayerWins();
+		gameReset();
+	}
+	else if (userTotal > randomNum) {
+		ifThePlayerLosses();
+		gameReset();
+	}
+
+});
+
+});
+
